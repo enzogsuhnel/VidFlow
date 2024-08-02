f46f89798fa5f8de77a36a4237669cb543d1ae4d
enzogsuhnel <enzogsuhnel@gmail.com>
2024-08-02 12:08:22 -0300

    videos (correction)

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const containerVideos = document.querySelector('.videos__container');
+const containerVideos = document.querySelector(".videos__container");
 
 const api = fetch("http://localhost:3000/videos")
 .then(res => res.json())
-.then((videos) => 
-    videos.foreach((video) => {
+.then((videos) =>
+    videos.forEach((video)=> {
         containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descircao-video">
-                <img class="img-canal" src="${video.imgaem}" alt="Logo do canal">
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
                 <h3 class="titulo-video">${video.titulo}</h3>
                 <p class="titulo-canal">${video.descricao}</p>
             </div>
-            </li>
+        </li>
         `;
     })
 )
-.catch((error)=> {
-    containerVideos.innerHTML = `
-    <p> Houve um erro para carregar os vídeos: ${error}! </p>
-    `;
+.catch((error) => {
+    containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`;
 })
